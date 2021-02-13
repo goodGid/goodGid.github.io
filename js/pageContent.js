fa726e0e47c57d292df6894e47f7d6abdd3f1d05
goodGid <hello.goodgid@gmail.com>
2021-02-13 21:58:38 +0900

    Add pageCounting.js

--- js/pageContent.js
+++ js/pageContent.js
@@ -6,6 +6,7 @@
  * 그렇지 않으면 스타일을 취소하십시오.
  */
 (function() {
+    increaseAccessCount(document.URL)
 
     if (window.innerWidth > 770) {
 
---
+++ js/pageCounting.js
@@ -0,0 +1,21 @@
+function increaseAccessCount(currentUrl){
+    var xhr = new XMLHttpRequest();
+
+    var data = {
+        accessUrl: currentUrl
+    };
+
+    xhr.onload = function() {
+    if (xhr.status === 200 || xhr.status === 201) {
+        console.log(xhr.responseText);
+    } else {
+        console.error(xhr.responseText);
+    }
+    };
+
+    // xhr.open("POST","http://localhost:8080/increase/view-count");
+    xhr.open("POST", "https://goodgid.ga/increase/view-count");
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
+}
